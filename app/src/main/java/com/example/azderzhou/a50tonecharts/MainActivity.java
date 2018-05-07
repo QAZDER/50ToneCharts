8d694fe7b1275f539c70c859c99b8e925046a561
Azder <zhou61226122@qq.com>
2018-05-07 23:24:34 +0800

    first commit

---
+++ app/src/main/java/com/example/azderzhou/a50tonecharts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.azderzhou.a50tonecharts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
